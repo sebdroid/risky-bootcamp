fb7dc3c834834efdbcc92da3037f5f4c3a75fd2e
sebdroid <me@sebjo.se>
2021-09-16 15:25:24 +0100

    update

--- app.py
+++ app.py
@@ -1,37 +1,38 @@
-import os, random, ast
+import os, random, ast, unittest
 
 from flask import Flask, render_template, request, make_response
-
-import unittest
-
-from flask.wrappers import Response
 
 class UnitTests(unittest.TestCase):
 
     def test_generatessixnumbers(self):
         self.assertEqual(len(list), 6)
 
-    def test_uniqueNumbers(self):
-        flag = 0
-        flag = len(set(list)) == len(list)
+    # def test_uniqueNumbers(self):
+    #     flag = 0
+    #     flag = len(set(list)) == len(list)
 
-        if(flag):
-            return(True)
+    #     if(flag):
+    #         return(True)
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def generateNumber():
-    if isinstance(request.cookies.get('randomNumbers'), str):
+    cookie = []
+
+    if isinstance(request.cookies.get('randomNumbers'), str) and request.method == 'GET':
         cookie = ast.literal_eval(request.cookies.get('randomNumbers'))
-        if isinstance(cookie, list):
-            print(cookie)
+        if not isinstance(cookie, list):
+            cookie = []
+    
 
     newNumbers = random.sample(range(1, 60), 6)
     newNumbers.sort()
+    lottoNumbers=" ".join(str(x) for x in newNumbers)
 
-    response = make_response(render_template('index.html', numbers=" ".join(str(x) for x in newNumbers), cookies=cookie))
-    response.set_cookie('randomNumbers', str(newNumbers))
+    response = make_response(render_template('index.html', numbers=lottoNumbers, cookies=cookie))
+    cookie.append(lottoNumbers)
+    response.set_cookie('randomNumbers', str(cookie))
     return response
  
